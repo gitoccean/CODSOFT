05f29a77c71f9419cabdd9dbdd11c65bf0025667
gitoccean <136047676+gitoccean@users.noreply.github.com>
2023-10-07 00:12:26 +0500

    To-Do List Application (Add, Display, Update, Delete, Exit)

--- TASK_1.py
+++ TASK_1.py
@@ -7,54 +7,56 @@
 
 task_list = []
 task_id = 1
+
 while True:
-    print('''  To-Do List Application
-press 1 to add a task
-press 2 to see tasks list
-press 3 to update by mark task as completed
-press 4 to delete your task
-press 5 to exist''')
-    choice = int(input("Enter your Choice:  "))
+    print('''To-Do List Application
+    Press 1 to add a task
+    Press 2 to see task list
+    Press 3 to mark a task as completed
+    Press 4 to delete a task
+    Press 5 to exit''')
+
+    choice = int(input("Enter your choice: "))
+
     if choice == 1:
-        task_name = str(input("Enter your task:  "))
-        task_priority = str(input("Enter task priority (High,Medium,low):"))
+        task_name = str(input("Enter your task: "))
+        task_priority = str(input("Enter task priority (High, Medium, Low): "))
         task_status = "not done"
-        task = (task_id,task_name, task_priority, task_status)
+        task = [task_id, task_name, task_priority, task_status]
         task_list.append(task)
-        print(f"Your {task[1]} task added!")
+        print(f"Your '{task[1]}' task has been added!")
         task_id += 1
     elif choice == 2:
         if not task_list:
-            print("there is no task")
+            print("There are no tasks.")
         else:
-            print("-current tasks-")
+            print("- Current Tasks -")
             for task in task_list:
-                print(f"Id: {task[0]},Task:{task[1]}, priority:{task[2]}, Status:{task[3]},")
+                print(f"Id: {task[0]}, Task: {task[1]}, Priority: {task[2]}, Status: {task[3]}")
     elif choice == 3:
-        mark_task = str(input("Enter the name of task to mark as completed:  "))
-        update_task = []
+        mark_task = str(input("Enter the name of the task to mark as completed: "))
         task_found = False
         for task in task_list:
             if task[1] == mark_task:
-                task[1] = "done"
-                print(f"Task has been Marked")
+                task[3] = "done"
+                print("Task has been marked as completed.")
                 task_found = True
-            else:
-                print("There is no such task")
-            update_task.append(task)
-        task_list = update_task
+                break
+        if not task_found:
+            print("No task found with that name.")
     elif choice == 4:
-        name_del_task = str(input("Enter the task name you want to delete:  "))
-        task_del = False
+        name_del_task = str(input("Enter the task name you want to delete: "))
+        task_found = False
         for task in task_list:
             if task[1] == name_del_task:
                 task_list.remove(task)
-                print(f"Your task has been deleted")
-                task_del = True
-            else:
-                print("there is no such task")
+                print(f"Your '{name_del_task}' task has been deleted.")
+                task_found = True
+                break
+        if not task_found:
+            print("No task found with that name.")
     elif choice == 5:
-        print('Thanks for using our To-Do List app....')
+        print('Thanks for using our To-Do List app.')
         break
     else:
-        print('Invalid Input please choose from given functions')
+        print('Invalid input. Please choose from the given options.')
